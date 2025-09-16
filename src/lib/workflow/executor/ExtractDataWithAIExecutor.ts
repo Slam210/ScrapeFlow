b6990bac7b6c2417cdf2fed4894dcd71f3056789
Steven Lam <stevenl.inbox@gmail.com>
2025-09-16 14:56:58 -0700

    Modified the Extract Data with AI node to use OpenAI instead of mock data

--- src/lib/workflow/executor/ExtractDataWithAIExecutor.ts
+++ src/lib/workflow/executor/ExtractDataWithAIExecutor.ts
@@ -2,6 +2,7 @@ import { ExecutionEnvironment } from "@/types/executor";
 import { ExtractDataWithAITask } from "../task/ExtractDataWithAI";
 import prisma from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
+import OpenAI from "openai";
 
 /**
  * Executes a click on a page element defined by the task's "Selector" input.
@@ -54,13 +55,57 @@ export async function ExtractDataWithAIExecutor(
       return false;
     }
 
-    const mockExtractedData = {
-      usernameSelector: "#username",
-      passwordSelector: "#password",
-      loginSelector: "body > div > form > input.btn.btn-primary",
-    };
+    // const mockExtractedData = {
+    //   usernameSelector: "#username",
+    //   passwordSelector: "#password",
+    //   loginSelector: "body > div > form > input.btn.btn-primary",
+    // };
+    // environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
 
-    environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
+    const openai = new OpenAI({
+      apiKey: plainCredentialValue,
+    });
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text.",
+        },
+        {
+          role: "user",
+          content: content,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      // Higher makes it more creative and random
+      // Lower makes it more focused and deterministic
+      temperature: 1,
+    });
+
+    if (!response) {
+      environment.log.error("Issue with API call to OpenAI");
+      return false;
+    }
+
+    environment.log.info(`Prompt tokens: ${response.usage?.prompt_tokens}`);
+    environment.log.info(
+      `Completion tokens: ${response.usage?.completion_tokens}`
+    );
+
+    const result = response.choices[0].message?.content;
+
+    if (!result) {
+      environment.log.error("Empty response with AI");
+      return false;
+    }
+
+    environment.setOutput("Extracted data", JSON.stringify(response));
 
     return true;
   } catch (error) {
